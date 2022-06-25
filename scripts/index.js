dcb07acc125084650bf70b7345f4953490ffb716
Elena Delikanova <yvdelik2@mts.ru>
2022-06-25 23:55:51 +0300

    [feat] небольшой рефакторинг

--- scripts/index.js
+++ scripts/index.js
@@ -24,33 +24,58 @@ const initialCards = [
     link: './images/well_courtyard.jpg',
   },
 ];
+
+const photosGallary = document.querySelector('.photos__gallary');
+const addPhotoPopup = document.querySelector('.popup-add-photo');
+const addPhotoForm = document.querySelector('.add-photo-form');
+const closePopupButtons = document.querySelectorAll('.popup__close-button');
+const addPhotoButton = document.querySelector('.profile__add-button');
+const editInfoPopup = document.querySelector('.popup-edit-info');
+const editInfoButton = document.querySelector('.profile__edit-button');
+const profileNameElement = document.querySelector('.profile__name');
+const profileCaptionElement = document.querySelector('.profile__caption');
+const openPhotoPopup = document.querySelector('.popup-photo');
+const editInfoForm = editInfoPopup.querySelector('.edit-form');
+const profileNameInInput = editInfoForm.querySelector('#profile-name');
+const profileCaptionInInput = editInfoForm.querySelector('#profile-caption');
+const saveProfileInfoButton = editInfoPopup.querySelector('.form__save-button');
+const photoElements = photosGallary.querySelectorAll('.photos__photo');
+
 function togglePopup(popup) {
   popup.classList.toggle('popup_opened');
 }
 
-const photosGallary = document.querySelector('.photos__gallary');
 function addNewPhotoToGallary(params) {
   const photoCardTemplate = document.querySelector('#photo-card').content;
   const photoCardElement = photoCardTemplate.querySelector('.photos__photo-card').cloneNode(true);
-  photoCardElement.querySelector('.photos__photo').src = params.link;
-  photoCardElement.querySelector('.photos__photo-caption').textContent = params.name;
-  photoCardElement.querySelector('.photos__photo').alt = `Фотография ${params.name}`;
-  photoCardElement.querySelector('.photos__delete-button').addEventListener('click', (event) => {
+  const image = photoCardElement.querySelector('.photos__photo');
+  const caption = photoCardElement.querySelector('.photos__photo-caption');
+  const deleteButton = photoCardElement.querySelector('.photos__delete-button');
+  const likeButton = photoCardElement.querySelector('.photos__like-button');
+  caption.textContent = params.name;
+  image.src = params.link;
+  image.alt = `Фотография ${params.name}`;
+  image.addEventListener('click', (event) => {
+    const photoElementInPopup = openPhotoPopup.querySelector('.popup-photo__photo');
+    const photoCaptionElement = openPhotoPopup.querySelector('.popup-photo__photo-caption');
+    photoElementInPopup.src = event.target.src;
+    photoElementInPopup.alt = event.target.alt;
+    photoCaptionElement.textContent = event.target.parentElement.querySelector('.photos__photo-caption').textContent;
+    /* При длинной подписи к фото ее растягивает больше, чем на ширину экрана. Если задать максимальный размер подписи в 75vw получается не очень красиво, если картинка узкая. Google дает в качестве варианта использование display: table для figure и display: table-caption для подписи, но это не помогло. Единственное, что мне пришло в голову -- высчитать вот так. Наверное, есть более изящный способ. Подскажите, пожалуйста, если вам не трудно. Спасибо!*/
+    photoCaptionElement.style.width = photoElementInPopup.clientWidth + 'px';
+    togglePopup(openPhotoPopup);
+  });
+  deleteButton.addEventListener('click', (event) => {
     const deletedPhoto = event.target.parentElement;
     deletedPhoto.remove();
   });
-  photoCardElement.querySelector('.photos__like-button').addEventListener('click', (event) => {
+  likeButton.addEventListener('click', (event) => {
     event.target.classList.toggle('photos__like-button_active');
   });
   photosGallary.prepend(photoCardElement);
 }
 
-initialCards.forEach((cardInfo) => {
-  addNewPhotoToGallary({ name: cardInfo.name, link: cardInfo.link });
-});
 
-const addPhotoPopup = document.querySelector('.popup-add-photo');
-const addPhotoForm = document.querySelector('.add-photo-form');
 addPhotoForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const newPhotoCaptureInInput = event.target.querySelector('#photo-capture');
@@ -64,26 +89,15 @@ addPhotoForm.addEventListener('submit', (event) => {
   togglePopup(addPhotoPopup);
 });
 
-const closePopupButtons = document.querySelectorAll('.popup__close-button');
 closePopupButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
     togglePopup(event.target.parentElement.parentElement);
   });
 });
 
-const addPhotoButton = document.querySelector('.profile__add-button');
 addPhotoButton.addEventListener('click', () => {
   togglePopup(addPhotoPopup);
 });
-
-const editInfoPopup = document.querySelector('.popup-edit-info');
-const editInfoForm = editInfoPopup.querySelector('.edit-form');
-const editInfoButton = document.querySelector('.profile__edit-button');
-
-const profileNameInInput = editInfoForm.querySelector('#profile-name');
-const profileCaptionInInput = editInfoForm.querySelector('#profile-caption');
-const profileNameElement = document.querySelector('.profile__name');
-const profileCaptionElement = document.querySelector('.profile__caption');
 
 editInfoButton.addEventListener('click', () => {
   profileNameInInput.value = profileNameElement.textContent;
@@ -91,7 +105,6 @@ editInfoButton.addEventListener('click', () => {
   togglePopup(editInfoPopup);
 });
 
-const saveProfileInfoButton = editInfoPopup.querySelector('.form__save-button');
 editInfoForm.addEventListener('submit', (event) => {
   event.preventDefault();
   profileNameElement.textContent = profileNameInInput.value;
@@ -99,15 +112,6 @@ editInfoForm.addEventListener('submit', (event) => {
   togglePopup(editInfoPopup);
 });
 
-const photoElements = photosGallary.querySelectorAll('.photos__photo');
-const openPhotoPopup = document.querySelector('.popup-photo');
-photoElements.forEach((photoElement) => {
-  photoElement.addEventListener('click', (event) => {
-    const photoElementInPopup = openPhotoPopup.querySelector('.popup-photo__photo');
-    const photoCaptionElement = openPhotoPopup.querySelector('.popup-photo__photo-caption');
-    photoElementInPopup.src = event.target.src;
-    photoElementInPopup.alt = event.target.alt;
-    photoCaptionElement.textContent = event.target.parentElement.querySelector('.photos__photo-caption').textContent;
-    togglePopup(openPhotoPopup);
-  });
+initialCards.forEach((cardInfo) => {
+  addNewPhotoToGallary({ name: cardInfo.name, link: cardInfo.link });
 });
